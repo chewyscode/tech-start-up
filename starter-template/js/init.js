2ec3b49724b8b3ff580c55ccfd0cef188e4dcbf6
chu-young <ttheko101@gmail.com>
2020-12-08 12:40:53 +0200

    added image carousel, scrollspy and tooltip. Website is now complete

--- starter-template/js/init.js
+++ starter-template/js/init.js
@@ -1,7 +1,11 @@
 (function($){
   $(function(){
-
+    $('.carousel.carousel-slider').carousel({
+      fullWidth: true,
+      indicators: true
+    })
     $('.sidenav').sidenav();
-
+$('.tooltipped').tooltip();
+$('.scrollspy').scrollSpy();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
